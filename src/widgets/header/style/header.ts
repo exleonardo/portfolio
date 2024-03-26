00757c879b2c3b61aa4ade8bac55bc5261c24a36
Alexander Khomenok <exleonardo@mail.ru>
2024-03-26 14:39:12 +0300

    style: refactor

--- src/widgets/header/style/header.ts
+++ src/widgets/header/style/header.ts
@@ -1,13 +1,17 @@
 import styled from 'styled-components'
 
 export const StyledHeader = styled.header`
-  margin-top: 30px;
-  height: 44px;
+  height: 60px;
   width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
   padding: 0px 80px 0px 80px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  background: rgb(52, 52, 52);
   align-items: center;
 `
 export const StyledUl = styled.ul`
