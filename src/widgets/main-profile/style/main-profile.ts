da01a4335c551dc3dacb304b425323020c699233
Alexander Khomenok <exleonardo@mail.ru>
2024-03-28 16:39:24 +0300

    style: add main-profile.ts

---
+++ src/widgets/main-profile/style/main-profile.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const MainStyle = styled.main`
+  padding-top: 100px;
+  margin-left: 80px;
+  margin-right: 80px;
+  align-items: flex-start;
+  display: flex;
+
+  justify-content: space-between;
+`
