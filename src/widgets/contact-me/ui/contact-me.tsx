3dffa4a7f5dcab2f5b737615400ae2d3f87194f7
Alexander Khomenok <exleonardo@mail.ru>
2024-03-28 17:01:56 +0300

    feat: add pattern

--- src/widgets/contact-me/ui/contact-me.tsx
+++ src/widgets/contact-me/ui/contact-me.tsx
@@ -8,21 +8,30 @@ import {
   ContactMeTitle,
   UserDataSend,
   UserDescriptionSend,
+  useContactMe,
 } from '@/widgets/contact-me'
 
 export const ContactMe = () => {
+  const { form, sendEmail } = useContactMe()
+
   return (
-    <ContactMeBlock>
+    <ContactMeBlock id={'contactMe'}>
       <ContactMeTitle>Contact Me</ContactMeTitle>
       <ContactInfo> Cultivating Connections: Reach Out and Connect with Me</ContactInfo>
-      <BlockForm>
+      <BlockForm onSubmit={sendEmail} ref={form}>
         <UserDataSend>
-          <Input title={'Email'} type={'email'} />
-          <Input title={'Name'} type={'text'} />
-          <Input title={'Phone Number'} type={'tel'} />
+          <Input
+            name={'user_email'}
+            pattern={`^((([0-9A-Za-z]{1}[-0-9A-z\\.]{0,30}[0-9A-Za-z]?)|([0-9А-Яа-я]{1}[-0-9А-я\\.]{0,30}[0-9А-Яа-я]?))@([-A-Za-z]{1,}\\.){1,}[-A-Za-z]{2,})$`}
+            required
+            title={'Email'}
+            type={'email'}
+          />
+          <Input name={'user_name'} required title={'Name'} type={'text'} />
+          <Input name={'user_phone'} required title={'Phone Number'} type={'tel'} />
         </UserDataSend>
         <UserDescriptionSend>
-          <Textarea title={'Project Details'} />
+          <Textarea name={'message'} required title={'Project Details'} />
           <Button type={'submit'} variant={'secondary'}>
             send
           </Button>
