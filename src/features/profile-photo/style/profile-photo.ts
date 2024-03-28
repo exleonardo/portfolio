f3500f15612ce1232b6c95c1c9f5e486364392ee
Alexander Khomenok <exleonardo@mail.ru>
2024-03-28 16:36:55 +0300

    style: add profile-photo.ts

---
+++ src/features/profile-photo/style/profile-photo.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const BlockPhoto = styled.div`
+  width: 618px;
+  height: 781px;
+  display: flex;
+  align-items: flex-end;
+`
+export const UserPhoto = styled.img`
+  border-radius: 244px;
+  width: 520px;
+  height: 781px;
+  z-index: 10;
+`
+export const BackGround = styled.div`
+  width: 618px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  height: 618px;
+  z-index: 20;
+  border-radius: 50%;
+  background: var(--color-dark-600);
+`
