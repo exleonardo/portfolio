41d6ca056d3789f4d69bdfa18c8a7ec6ed98b3c0
Alexander Khomenok <exleonardo@mail.ru>
2024-03-28 16:57:35 +0300

    feat: refactor

--- src/features/particle/ui/particle.tsx
+++ src/features/particle/ui/particle.tsx
@@ -1,86 +1,8 @@
-import { useEffect, useMemo, useState } from 'react'
-
-import { type Container, type ISourceOptions } from '@tsparticles/engine'
-import Particles, { initParticlesEngine } from '@tsparticles/react'
-import { loadSlim } from '@tsparticles/slim'
+import { useParticle } from '@/features/particle'
+import Particles from '@tsparticles/react'
 
 export const Particle = () => {
-  const [init, setInit] = useState(false)
-
-  useEffect(() => {
-    initParticlesEngine(async engine => {
-      await loadSlim(engine)
-    }).then(() => {
-      setInit(true)
-    })
-  }, [])
-  const particlesLoaded = async (container?: Container): Promise<void> => {
-    console.log(container)
-  }
-  const options: ISourceOptions = useMemo(
-    () => ({
-      detectRetina: true,
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onHover: {
-            enable: true,
-            mode: 'repulse',
-            parallax: { enable: true, smooth: 200 },
-          },
-        },
-        modes: {
-          push: {
-            quantity: 1,
-          },
-          repulse: {
-            distance: 50,
-            duration: 1,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: 'rgb(123,142,178)',
-        },
-        effect: { fill: true },
-        links: {
-          color: 'rgb(82,238,191)',
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        move: {
-          direction: 'none',
-          enable: false,
-          outModes: {
-            default: 'split',
-          },
-          random: true,
-          speed: 500,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 200,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          fill: true,
-          type: 'circle',
-        },
-        size: {
-          value: { max: 5, min: 1 },
-        },
-      },
-      smooth: true,
-    }),
-    []
-  )
+  const { init, options, particlesLoaded } = useParticle()
 
   if (init) {
     return (
