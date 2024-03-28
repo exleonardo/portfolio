03f6b38914d4c219189a8aeb4f3e87f3b8525df0
Alexander Khomenok <exleonardo@mail.ru>
2024-03-28 16:53:45 +0300

    style: refactor button

--- src/shared/button/ui/Button.tsx
+++ src/shared/button/ui/Button.tsx
@@ -4,15 +4,16 @@ import { ComponentPropsWithoutRef, ElementType } from 'react'
 import { ButtonStyled } from '@/shared/button'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
   children?: React.ReactNode
   fullWidth?: boolean
   variant: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
-export const Button = ({ children, fullWidth, variant, ...rest }: ButtonProps) => {
-  return (
-    <ButtonStyled {...rest} fullWidth={fullWidth} variant={variant}>
-      {children}
-    </ButtonStyled>
-  )
+export const Button = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+) => {
+  const { fullWidth, variant = 'primary', ...rest } = props
+
+  return <ButtonStyled {...rest} fullWidth={fullWidth} variant={variant} />
 }
