61f5d1a800be95d3bf47937814c6e8bfbb10eb21
Alexander Khomenok <exleonardo@mail.ru>
2024-03-30 21:48:26 +0300

    style: add responsive

--- src/widgets/button-block/style/button-block.ts
+++ src/widgets/button-block/style/button-block.ts
@@ -3,5 +3,13 @@ import styled from 'styled-components'
 export const ButtonCV = styled.div`
   display: flex;
   margin-top: 60px;
+  width: 100%;
   gap: 24px;
+  @media screen and (width <= 992px) {
+    flex-wrap: wrap;
+    padding-top: 1rem;
+    max-width: 450px;
+    width: 100%;
+    margin: 0 auto;
+  }
 `
