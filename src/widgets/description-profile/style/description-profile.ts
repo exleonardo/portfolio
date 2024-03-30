79b96cac923579be6a2dbb2d2726a617b277611b
Alexander Khomenok <exleonardo@mail.ru>
2024-03-30 21:45:08 +0300

    style: add responsive

--- src/widgets/description-profile/style/description-profile.ts
+++ src/widgets/description-profile/style/description-profile.ts
@@ -15,7 +15,7 @@ export const Name = styled.h2`
 
   color: var(--color-light-100);
   width: max-content;
-  font-size: var(--font-size-xxxl);
+
   font-weight: var(--font-weight-bold);
   line-height: var(--line-height-l);
 
@@ -23,23 +23,26 @@ export const Name = styled.h2`
 `
 export const Developer = styled.h1`
   position: relative;
-  width: max-content;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  padding: 0px;
+
+  word-break: break-word;
   z-index: 10;
 
-  margin: 24px 0px;
+  margin: 24px 0;
 
   background: linear-gradient(109.57deg, rgb(152, 67, 0), rgb(253, 111, 0), rgb(202, 89, 0));
-  text-shadow: 0px 0px 250px rgb(253, 111, 0);
+  text-shadow: 0 0 250px rgb(253, 111, 0);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   background-clip: text;
 
-  font-size: 70px;
   font-weight: var(--font-weight-bolder);
-  line-height: 84px;
+  line-height: 55px;
+`
+export const ProfileBlock = styled.div`
+  @media screen and (width <= 992px) {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-direction: column;
+  }
 `
