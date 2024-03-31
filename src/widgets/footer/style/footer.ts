d765088fe27df3809a2b02b1d1096f43ff20b5ee
Alexander Khomenok <exleonardo@mail.ru>
2024-03-31 15:51:24 +0300

    style: add responsive

--- src/widgets/footer/style/footer.ts
+++ src/widgets/footer/style/footer.ts
@@ -15,6 +15,8 @@ export const FooterContent = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  background: inherit;
+
   gap: 40px;
   margin-left: 80px;
   margin-right: 80px;
@@ -38,7 +40,9 @@ export const UserContact = styled.div`
   font-weight: var(--font-weight-bold);
   line-height: var(--line-height-m);
   gap: 10px;
+  position: relative;
 
+  background: inherit;
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
@@ -60,4 +64,12 @@ export const Description = styled.div`
   display: flex;
   justify-content: center;
   border-top: 1px solid var(--color-light-100);
+`
+export const Mail = styled.div`
+  display: flex;
+  gap: 10px;
+`
+export const Phone = styled.div`
+  display: flex;
+  gap: 10px;
 `
