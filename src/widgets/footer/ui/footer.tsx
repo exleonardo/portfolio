484a28127c9b89bb59d08d562e4f11101f3c2730
Alexander Khomenok <exleonardo@mail.ru>
2024-03-31 15:54:26 +0300

    feat: refactor block

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -1,6 +1,6 @@
 import { MailIcon } from '@/assets/icons/mail'
 import { PhoneIcon } from '@/assets/icons/phone'
-import { Description, FooterBlock, FooterContent, UserContact } from '@/widgets/footer'
+import { Description, FooterBlock, FooterContent, Mail, Phone, UserContact } from '@/widgets/footer'
 import { IconBlock } from '@/widgets/icon-block'
 import { Menu } from '@/widgets/menu'
 
@@ -11,10 +11,14 @@ export const Footer = () => {
         <Menu />
         <IconBlock />
         <UserContact>
-          <MailIcon fill={'grey'} />
-          exleonardo31@gmail.com
-          <PhoneIcon />
-          +375(29)-706-85-11
+          <Mail>
+            <MailIcon fill={'grey'} />
+            exleonardo31@gmail.com
+          </Mail>
+          <Phone>
+            <PhoneIcon />
+            +375(29)-706-85-11
+          </Phone>
         </UserContact>
         <Description> 2024 ALL RIGHT RESERVED</Description>
       </FooterContent>
