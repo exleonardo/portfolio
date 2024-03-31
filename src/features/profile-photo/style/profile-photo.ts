18616b68b707fab894d3b886484edc3356641fc3
Alexander Khomenok <exleonardo@mail.ru>
2024-03-31 15:50:32 +0300

    style: add responsive

--- src/features/profile-photo/style/profile-photo.ts
+++ src/features/profile-photo/style/profile-photo.ts
@@ -2,22 +2,39 @@ import styled from 'styled-components'
 
 export const BlockPhoto = styled.div`
   width: 618px;
-  height: 781px;
-  display: flex;
-  align-items: flex-end;
+  height: max-content;
+
+  padding-top: 150px;
+  @media screen and (width <= 992px) {
+    margin: 0 auto;
+    width: 418px;
+    height: max-content;
+    padding-top: 100px;
+  }
+  @media screen and (width <= 576px) {
+  }
 `
 export const UserPhoto = styled.img`
   border-radius: 244px;
-  width: 520px;
-  height: 781px;
-  z-index: 10;
+  min-width: 200px;
+  max-width: 520px;
+  width: 100%;
+  z-index: 30;
 `
+
 export const BackGround = styled.div`
-  width: 618px;
+  overflow: visible;
+  min-width: 275px;
+
+  padding-left: 49px;
+  padding-right: 49px;
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  height: 618px;
+
+  min-height: 275px;
+  aspect-ratio: 1/1;
+
   z-index: 20;
   border-radius: 50%;
   background: var(--color-dark-600);
