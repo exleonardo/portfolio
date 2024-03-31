c1b6d758f91b6f8925f0622d1118652b24fa640c
Alexander Khomenok <exleonardo@mail.ru>
2024-03-31 15:52:19 +0300

    style: add responsive

--- src/widgets/about-me/style/about-me.ts
+++ src/widgets/about-me/style/about-me.ts
@@ -3,5 +3,12 @@ import styled from 'styled-components'
 export const AboutMeBlock = styled.div`
   display: flex;
   flex-direction: column;
+  max-width: max-content;
+  width: 100%;
   margin-top: 100px;
+  flex-shrink: 1;
+  @media screen and (width <= 992px) {
+    margin-top: 0;
+    max-width: 100%;
+  }
 `
