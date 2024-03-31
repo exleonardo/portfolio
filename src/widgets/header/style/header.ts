47d86f74bb46fc130e85fda16f15ecce8ff12380
Alexander Khomenok <exleonardo@mail.ru>
2024-03-31 15:51:16 +0300

    style: add responsive

--- src/widgets/header/style/header.ts
+++ src/widgets/header/style/header.ts
@@ -4,24 +4,37 @@ import styled from 'styled-components'
 
 export const StyledHeader = styled.header`
   height: 60px;
-  width: 100%;
+  width: 100vw;
   position: fixed;
   top: 0;
   left: 0;
   z-index: 100;
   padding: 0 80px 0 80px;
   display: flex;
-  flex-direction: row;
+
   justify-content: space-between;
   background: var(--color-dark-600);
   align-items: center;
+  @media screen and (width <= 992px) {
+    padding: 0 20px 0 20px;
+  }
 `
 export const StyledUl = styled.ul`
+  flex-wrap: wrap;
   display: flex;
+  @media screen and (width <= 992px) {
+    flex-direction: column;
+  }
 `
 export const ElementLink = styled.li`
   &:not(:first-child) {
-    margin-left: 60px;
+    margin-left: 3.75rem;
+  }
+
+  @media screen and (width <= 992px) {
+    &:not(:first-child) {
+      margin-left: 0;
+    }
   }
 `
 export const NavLink = styled(Link)`
@@ -36,6 +49,10 @@ export const NavLink = styled(Link)`
     font-size: var(--font-size-xl);
     font-weight: 700;
     line-height: var(--line-height-m);
+
+    &:hover {
+      color: var(--color-warning-900);
+    }
   }
 
   &:hover {
@@ -45,6 +62,9 @@ export const NavLink = styled(Link)`
   font-size: var(--font-size-xl);
   font-weight: var(--font-weight-normal);
   line-height: var(--line-height-m);
+  @media screen and (width <= 992px) {
+    flex-wrap: wrap;
+  }
 `
 export const LogoLink = styled.a`
   & > svg {
@@ -55,5 +75,46 @@ export const LogoLink = styled.a`
       cursor: pointer;
       fill: var(--color-light-300);
     }
+  }
+
+  @media screen and (width <= 576px) {
+    display: none;
+  }
+`
+export const Burger = styled.button`
+  cursor: pointer;
+  @media screen and (width >= 576px) {
+    display: none;
+  }
+`
+
+export const ModalNav = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+
+  top: 0;
+  left: 0;
+
+  right: 0;
+  bottom: 0;
+  background: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: white;
+  gap: 30px;
+
+  & > svg {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+`
+
+export const ButtonLink = styled.div`
+  @media screen and (width <= 576px) {
+    display: none;
   }
 `
